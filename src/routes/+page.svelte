04a4f86e2a3b730198aeea9d810192ac1b75f001
adnksharp <darkmark404@protonmail.com>
2025-07-03 17:22:19 -0600

    refactor (+page): update main window

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,23 @@
 		#about {
 			scroll-snap-align: start;
 			scroll-snap-stop: always;
+			max-width: calc(16.0 / 9.0 * 100vh);
+			margin-inline: auto;
+			height: 100vh;
+			text-align: center;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+		}
+
+		#header h2,
+		#projects h2,
+		#about h2 {
+			text-align: center;
+			font-size: 2.5em;
+			width: 100%;
+			margin: 40px 0 0 0;
 		}
 	</style>
 	<ATF />
